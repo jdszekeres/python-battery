868ec8d2d2857e2098ca0af1809e0875989a4c9f
jdszekeres <69050063+jdszekeres@users.noreply.github.com>
2020-08-20 08:32:43 -0700

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 curl -K https://raw.githubusercontent.com/jdszekeres/python-battery/master/battery?token=AQOZ5T54DWIGHMUGKTXMMNK7H2KQQ -o battery
 chmod +x battery
+open https://github.com/jdszekeres/python-battery#%EF%B8%8Eonly-tested-om-mac%EF%B8%8E
